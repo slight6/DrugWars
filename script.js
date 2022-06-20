3157c1337b63d1546547948d6265e49a07890557
slight6 <admin@27pro.com>
2022-06-19 20:52:19 -0400

    Adding a little style

---
+++ script.js
@@ -0,0 +1,21 @@
+var name = "user name"
+init("this name")
+function init(name) {
+    console.log(name)
+
+}
+
+
+var rand = 
+{
+    rand20 : Math.random()*20,
+    rand10 : Math.random()*10,
+    rand5 : Math.random()*5
+
+
+};
+
+console.log(rand.rand10)
+console.log(rand.rand20)
+console.log(rand.rand5)
+
