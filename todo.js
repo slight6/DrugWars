d51b4d1227c9a530979016795085468a649c02ca
Jason Consiglio <admin@27pro.com>
2022-06-19 22:37:22 -0400

    added some really loose pseudocode

---
+++ todo.js
@@ -0,0 +1,22 @@
+
+/* This is a file that will just mainly be used for jotting down notes and pseudocode down as I get acclimated with javascript and developing this super simple game. */
+
+/* 
+
+Initialize the following :      
+                                days = 90
+                                location = 1 (New York City)
+                                cash on hand = $5000
+                                loanshark = $7500
+                                pockets = 100
+                                weapons = 2
+                                ammo = 20
+                                ludes = 10-40
+                                speed = 30-100
+                                weed = 90-300
+                                acid = 280-1200
+                                heroin = 1000-3000
+                                cocaine = 2500-5000
+                                special randoms (1 to randomly add a special, 1 to decide which one it is, 1 to figure out if it is up or down, 1 to decide by how much, then meeting certain requirements, decide what the news will be, as well as what the local news says, which may trigger a second special event, such as Yankees win the World Series, celebrations causing some drug prices to skyrocket. DEA truck crashes, freeing up tons of drugs in all categories.)
+
+*/
